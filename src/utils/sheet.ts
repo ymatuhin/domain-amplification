1027ec2946c7ef826a9c69eac4c18ec67b1cd574
ymatuhin <ym@ymatuhin.ru>
2022-07-07 14:24:24 +0300

    Fix adoptedStyleSheets insertion/deletion

--- src/utils/sheet.ts
+++ src/utils/sheet.ts
@@ -8,26 +8,17 @@ type Queue = { type: "add" | "remove"; rule: string };
 export class Sheet {
   log: ReturnType<typeof logger>;
   #name: string;
-  #sync: boolean;
-  #style: HTMLStyleElement;
-  #sheet: CSSStyleSheet;
+  #sync: boolean = false;
+  #sheet = new CSSStyleSheet();
   #queue: Queue[] = [];
 
   constructor(name: string, sync?: boolean) {
     this.log = logger(`sheet:${name}`);
     this.#name = name;
     this.#sync = sync ?? !checkInsideIframe();
+    this.start();
     this.handleQueue = rafThrottle(this.handleQueue.bind(this));
-    this.#style = this.createStyle(name);
-    this.#sheet = this.#style.sheet!;
     if (this.#sync) this.#load();
-  }
-
-  createStyle(name: string) {
-    const style = document.createElement("style");
-    style.classList.add(`sdm-${name}`);
-    document.documentElement.appendChild(style);
-    return style;
   }
 
   makeRule(initialRule: string) {
@@ -39,7 +30,7 @@ export class Sheet {
   addRule(rule: string, instant: boolean = false) {
     this.log("addRule", { rule, instant });
     if (instant) {
-      this.insertRule(rule);
+      this.#insertRule(rule);
       requestIdleCallback(() => this.#save());
     } else {
       this.#queue.push({ type: "add", rule });
@@ -50,7 +41,7 @@ export class Sheet {
   removeRule(rule: string, instant: boolean = false) {
     this.log("removeRule", { rule, instant });
     if (instant) {
-      this.deleteRule(rule);
+      this.#deleteRule(rule);
       requestIdleCallback(() => this.#save());
     } else {
       this.#queue.push({ type: "remove", rule });
@@ -58,14 +49,20 @@ export class Sheet {
     }
   }
 
-  async start() {
+  start() {
     this.log("start");
-    document.documentElement.appendChild(this.#style);
+    // @ts-ignore
+    if (document.adoptedStyleSheets.includes(this.#sheet)) return;
+    // @ts-ignore
+    document.adoptedStyleSheets = [...document.adoptedStyleSheets, this.#sheet];
   }
 
   pause() {
     this.log("pause");
-    this.#style.parentElement?.removeChild(this.#style);
+    // @ts-ignore
+    document.adoptedStyleSheets = document.adoptedStyleSheets.filter(
+      (sheet: any) => sheet !== this.#sheet,
+    );
   }
 
   clear() {
@@ -78,8 +75,8 @@ export class Sheet {
     if (this.#queue.length === 0) return;
     this.log("handleQueue");
     this.#queue.forEach(({ rule, type }) => {
-      if (type === "add") this.insertRule(rule);
-      if (type === "remove") this.deleteRule(rule);
+      if (type === "add") this.#insertRule(rule);
+      if (type === "remove") this.#deleteRule(rule);
     });
     this.#queue = [];
 
@@ -87,15 +84,14 @@ export class Sheet {
     requestIdleCallback(() => this.#save());
   }
 
-  insertRule(rule: string) {
+  #insertRule(rule: string) {
     const rulesArr = Array.from(this.#sheet.cssRules);
     if (rulesArr.some(({ cssText }) => cssText === rule)) return;
     this.log("insertRule", rule);
-    var index = this.#sheet.cssRules.length;
-    this.#sheet.insertRule(rule, index);
+    this.#sheet.insertRule(rule);
   }
 
-  deleteRule(rule: string) {
+  #deleteRule(rule: string) {
     const rulesArr = Array.from(this.#sheet.cssRules);
     const index = rulesArr.findIndex(({ cssText }) => cssText === rule);
     if (index >= 0) {
@@ -107,15 +103,14 @@ export class Sheet {
   #save() {
     const rules = Array.from(this.#sheet.cssRules);
     const text = rules.map((rule) => rule.cssText).join("\n");
-    this.log("save", { text });
+    this.log("save", text);
     localStorage.setItem(`sdm-sheet-${this.#name}`, text);
   }
 
   #load() {
     const saved = localStorage.getItem(`sdm-sheet-${this.#name}`);
-    this.log("load", { saved });
+    this.log("load", saved);
     // @ts-ignore
-    console.info(`🔥 this.#sheet`, this.#sheet);
     if (saved) this.#sheet.replaceSync(saved);
   }
 }
